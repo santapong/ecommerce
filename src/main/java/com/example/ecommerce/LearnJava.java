69d499625b06311768ea949223351227cbf92b50
Santapong <santapongsondhi@gmail.com>
2025-05-09 17:02:53 +0700

    update learning

---
+++ src/main/java/com/example/ecommerce/LearnJava.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce;
+
+// Import ArrayList class inside of the java.util package
+import java.util.ArrayList;
+// Import all classes inside of java.security package
+import java.security.*;
+// Java to illustrate calling of static members and methods without calling classname
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+public class LearnJava {
+
+}
