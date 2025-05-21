ec8e82f0c6a6e1579a2df7762195331be56c4b73
Santapong <santapongsondhi@gmail.com>
2025-05-21 14:07:57 +0700

    Add Hibernate

--- src/main/java/com/mycommerce/ecommerce/Application.java
+++ src/main/java/com/mycommerce/ecommerce/Application.java
@@ -1,11 +1,15 @@
 package com.mycommerce.ecommerce;
 
-import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    // public static void main(String[] args) {
+    //     SpringApplication.run(Application.class, args);
+    // }
+
+    public static void main(String[] args){
+        System.out.println("hello");
     }
 }
